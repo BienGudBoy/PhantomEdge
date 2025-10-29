0bfc49031b091170b31979e2b47a25576efbfae3
bien <bien2k4.email@protonmail.com>
2025-10-29 08:53:46 +0700

    Add PlayerCombat and DamageDealer systems

---
+++ Assets/Scripts/Combat/DamageDealer.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+public class DamageDealer : MonoBehaviour
+{
+    [Header("Damage Settings")]
+    [SerializeField] private int damageAmount = 10;
+    [SerializeField] private string targetTag = "Player";
+    [SerializeField] private bool destroyOnHit = true;
+    
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag(targetTag))
+        {
+            // Deal damage to target
+            if (targetTag == "Player")
+            {
+                HealthSystem health = other.GetComponent<HealthSystem>();
+                if (health != null)
+                {
+                    health.TakeDamage(damageAmount);
+                    
+                    // Play damage sound
+                    if (AudioManager.Instance != null)
+                    {
+                        AudioManager.Instance.PlayDamageSound();
+                    }
+                }
+            }
+            else if (targetTag == "Enemy")
+            {
+                EnemyHealth enemyHealth = other.GetComponent<EnemyHealth>();
+                if (enemyHealth != null)
+                {
+                    enemyHealth.TakeDamage(damageAmount);
+                }
+            }
+            
+            // Destroy projectile if specified
+            if (destroyOnHit)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
+
